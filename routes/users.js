9ab9c9d085e7d1937156f5edf30b31b00d586e7b
Cyri0 <nemestamas94@gmail.com>
2022-03-15 12:06:00 +0100

    my base node api for students

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,39 @@
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
 const router = express.Router()
 
-const users = [
-    {
-        firstName:"John",
-        lastName:"Doe",
-        age:25
-    },
-    {
-        firstName:"Jane",
-        lastName:"Doe",
-        age:24
-    }
-]
+let usersPath = path.join(path.resolve(), '/assets/user.json')
+
+let users = []
+
+let initializeUsers = () => {
+
+        fs.readFile(usersPath, 'utf8', (err, data) => {
+            if(err){
+                console.log('File not found!')
+            }else{
+                users = JSON.parse(data)
+            }
+        });
+}
+initializeUsers(usersPath)
+
 
 router.get('/', (req, res) => {
+    initializeUsers(usersPath)
     console.log(users)
     res.send(users)
 })
 
+
 router.post('/', (req, res) => {
-    console.log('POST ROUTE REACHED')
-    res.send('POST ROUTE REACHED')
+    const user = req.body
+
+    users.push(user)
+    fs.writeFileSync(usersPath, JSON.stringify(users))
+
+    res.send(`${user.firstName} ${user.lastName} (${user.age}) added!`)
 })
 
 export default router
