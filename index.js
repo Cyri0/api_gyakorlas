1ae41bb18f84e4a4309596bd8fdeaa2536dd78c9
Cyri0 <nemestamas94@gmail.com>
2022-03-15 11:15:58 +0100

    get request sample

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express()
 const PORT = 5000
 app.use(bodyParser.json())
 
-app.listen(PORT, () => { 
-    console.log(`Listening on port: http://localhost:${PORT}`)
+app.get('/', (req, res) => {
+    console.log("Somebody call the homepage...")
+    res.send('Hello from Homepage!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`)
 })
